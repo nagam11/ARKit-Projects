fb945b88e8d8aae9d6c74234e97c472e5f9e1e93
Marla Na <marla.narazani@aiesec.de>
2018-04-27 19:30:53 +0900

    fixed resizing and placing

--- Project 09 - ARMinecraft/ARMinecraft/ARMinecraft/ViewController.swift
+++ Project 09 - ARMinecraft/ARMinecraft/ARMinecraft/ViewController.swift
@@ -17,6 +17,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     var lastNode: SCNNode?
     // Variable to track the last touchpoint on touchpad
     fileprivate var lastPoint = CGPoint.zero
+    var planeNode = SCNNode()
+    // TODO: not needed maybe
+    //var vector = SCNVector3()
     // Variable to track the latest added cube size.
     var oldCubeSize = (0.1,0.1,0.1)
     // Variable to iterate through different cube skins
@@ -51,9 +54,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
         
         // Run the view's session
         sceneView.session.run(configuration)
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -62,6 +67,56 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        // 1
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        
+        // 2
+        let width = CGFloat(planeAnchor.extent.x)
+        let height = CGFloat(planeAnchor.extent.z/2)
+        let plane = SCNPlane(width: width, height: height)
+        
+        // 3
+        plane.materials.first?.diffuse.contents = UIColor.lightGray
+        
+        // 4
+        let planeNode = SCNNode(geometry: plane)
+        
+        // 5
+        let x = CGFloat(planeAnchor.center.x)
+        let y = CGFloat(planeAnchor.center.y)
+        let z = CGFloat(planeAnchor.center.z)
+        planeNode.position = SCNVector3(x,y,z)
+        planeNode.eulerAngles.x = -.pi / 2
+        
+        // 6
+        node.addChildNode(planeNode)
+        self.planeNode = node
+        //self.sceneView.scene.rootNode.addChildNode(planeNode)
+    }
+    
+    //TODO not sure it is needed
+  /*  func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        
+        // 1
+        guard let planeAnchor = anchor as?  ARPlaneAnchor,
+            let planeNode = node.childNodes.first,
+            let plane = planeNode.geometry as? SCNPlane
+            else { return }
+        
+        // 2
+        let width = CGFloat(planeAnchor.extent.x)
+        let height = CGFloat(planeAnchor.extent.z)
+        plane.width = width
+        plane.height = height
+        
+        // 3
+        let x = CGFloat(planeAnchor.center.x)
+        let y = CGFloat(planeAnchor.center.y)
+        let z = CGFloat(planeAnchor.center.z)
+        planeNode.position = SCNVector3(x, y, z)
+    }*/
 }
 
 // MARK: - Shapes
@@ -117,7 +172,7 @@ extension ViewController {
             //TODO
             
             if !self.lastPoint.equalTo(CGPoint.zero) {
-                print("Touchpad was touched")
+                //print("Touchpad was touched")
                 //print(point)
             }
             self.lastPoint = point
@@ -130,17 +185,33 @@ extension ViewController {
         }
         
         controller.appButton.valueChangedHandler = { (button: DDControllerButton, pressed: Bool) in
-            if pressed {
-                print("App button was pressed")
+            self.checking = ( self.checking.1 ,pressed ? true : false)
+            if self.checking.1 {
                 //Only interact with the object, if it's in the field of view.
                 if let pointOfView = self.sceneView.pointOfView {
                     let isVisible = self.sceneView.isNode(self.lastNode!, insideFrustumOf: pointOfView)
                     if isVisible {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: { // remove/replace ship after half a second to visualize collision
-                            //TODO place box on floor
-                        })
+                        if self.first {
+                            //TODO: Either delete of move
+                            self.lastNode?.removeFromParentNode()
+                            self.createCube(changeMaterial: false, multiplier: 0.0)
+                            
+                            let point = CGPoint(x: self.sceneView.frame.size.width / 2, y: self.sceneView.frame.size.height / 2); //Get a point at the middle of the screen
+                            let hitResults = self.sceneView.hitTest(point, types: .existingPlaneUsingExtent)
+                            
+                            if hitResults.count > 0, let firstHit = hitResults.first {
+                                print(Float(self.oldCubeSize.1/2))
+                                print(firstHit.worldTransform.columns.3.y)
+                                self.lastNode?.position = SCNVector3Make(firstHit.worldTransform.columns.3.x, firstHit.worldTransform.columns.3.y + Float(self.oldCubeSize.1/2), firstHit.worldTransform.columns.3.z)
+                            }
+                            print(self.lastNode?.position)
+                            print(self.planeNode.position)
+                        }
                     }
                 }
+            }
+            else  if (!self.checking.0 && !self.checking.1) {
+                self.first = true
             }
         }
         
@@ -157,12 +228,12 @@ extension ViewController {
                 if let pointOfView = self.sceneView.pointOfView {
                     let isVisible = self.sceneView.isNode(self.lastNode!, insideFrustumOf: pointOfView)
                     if isVisible {
-                            if self.first {
-                                self.lastNode?.removeFromParentNode()
-                                self.createCube(changeMaterial: false, multiplier: 0.1)
-                                self.oldCubeSize = (self.oldCubeSize.0 + 0.1, self.oldCubeSize.1 + 0.1, self.oldCubeSize.2 + 0.1)
-                                self.first = false
-                            }
+                        if self.first {
+                            self.lastNode?.removeFromParentNode()
+                            self.createCube(changeMaterial: false, multiplier: 0.1)
+                            self.oldCubeSize = (self.oldCubeSize.0 + 0.1, self.oldCubeSize.1 + 0.1, self.oldCubeSize.2 + 0.1)
+                            self.first = false
+                        }
                     }
                 }
             }
@@ -178,12 +249,12 @@ extension ViewController {
                 if let pointOfView = self.sceneView.pointOfView {
                     let isVisible = self.sceneView.isNode(self.lastNode!, insideFrustumOf: pointOfView)
                     if isVisible {
-                            if self.first {
-                                self.lastNode?.removeFromParentNode()
-                                self.createCube(changeMaterial: false, multiplier: (-0.1))
-                                self.oldCubeSize = (self.oldCubeSize.0 - 0.1, self.oldCubeSize.1 - 0.1, self.oldCubeSize.2 - 0.1)
-                                self.first = false
-                            }
+                        if self.first {
+                            self.lastNode?.removeFromParentNode()
+                            self.createCube(changeMaterial: false, multiplier: (-0.1))
+                            self.oldCubeSize = (self.oldCubeSize.0 - 0.1, self.oldCubeSize.1 - 0.1, self.oldCubeSize.2 - 0.1)
+                            self.first = false
+                        }
                     }
                 }
             }
