7dc4fd2e9b4eca3d9aba09c0d0cab5599bd687ce
Marla Na <marla.narazani@aiesec.de>
2018-05-04 20:14:08 +0900

    finished main functionalities

--- Project 09 - ARMinecraft/ARMinecraft/ARMinecraft/ViewController.swift
+++ Project 09 - ARMinecraft/ARMinecraft/ARMinecraft/ViewController.swift
@@ -13,26 +13,31 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
     
     @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var informationLabel: UILabel!
     // Variable to track the last added node in the scene
     var lastNode: SCNNode?
     // Variable to track the last touchpoint on touchpad
     fileprivate var lastPoint = CGPoint.zero
+    var firstPoint = CGPoint.zero
+    var lastPoi = CGPoint.zero
+    // TODO: should be optional
     var planeNode = SCNNode()
-    // TODO: not needed maybe
-    //var vector = SCNVector3()
     // Variable to track the latest added cube size.
     var oldCubeSize = (0.1,0.1,0.1)
     // Variable to iterate through different cube skins
     var materialsCode = 0
-    // Variable to make sure the signals from the controller are only executed once. Note: DayDream SDK delivers for one click, 9-10 notifications at once.
-    var first = true
+    // 3rd variable : Variable to make sure the signals from the controller are only executed once. Note: DayDream SDK delivers for one click, 9-10 notifications at once.
     // Variable to check two consecutive signals from the SDK. (false,false) : not pressed, (false, true) : pressed, (true, true) : not defined, (true, false) : not needed because we only execute once.
-    var checking = (false ,false)
+    var checking0 = (false ,false, true)
+    var checking = (false ,false, true)
+    var checking2 = (false ,false, true)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         discoverControllers()
+        
+        self.informationLabel.text = "VR Controller NOT connected."
         
         // Set the view's delegate
         sceneView.delegate = self
@@ -69,54 +74,21 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        // 1
+        // Detect planes
         guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
-        
-        // 2
         let width = CGFloat(planeAnchor.extent.x)
         let height = CGFloat(planeAnchor.extent.z/2)
         let plane = SCNPlane(width: width, height: height)
-        
-        // 3
         plane.materials.first?.diffuse.contents = UIColor.lightGray
-        
-        // 4
         let planeNode = SCNNode(geometry: plane)
-        
-        // 5
         let x = CGFloat(planeAnchor.center.x)
         let y = CGFloat(planeAnchor.center.y)
         let z = CGFloat(planeAnchor.center.z)
         planeNode.position = SCNVector3(x,y,z)
         planeNode.eulerAngles.x = -.pi / 2
-        
-        // 6
         node.addChildNode(planeNode)
         self.planeNode = node
-        //self.sceneView.scene.rootNode.addChildNode(planeNode)
     }
-    
-    //TODO not sure it is needed
-  /*  func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
-        
-        // 1
-        guard let planeAnchor = anchor as?  ARPlaneAnchor,
-            let planeNode = node.childNodes.first,
-            let plane = planeNode.geometry as? SCNPlane
-            else { return }
-        
-        // 2
-        let width = CGFloat(planeAnchor.extent.x)
-        let height = CGFloat(planeAnchor.extent.z)
-        plane.width = width
-        plane.height = height
-        
-        // 3
-        let x = CGFloat(planeAnchor.center.x)
-        let y = CGFloat(planeAnchor.center.y)
-        let z = CGFloat(planeAnchor.center.z)
-        planeNode.position = SCNVector3(x, y, z)
-    }*/
 }
 
 // MARK: - Shapes
@@ -161,6 +133,10 @@ extension ViewController {
     
     @objc func controllerDidConnect(_ notification: Notification) {
         print("VR Controller was connected successfully.")
+        self.informationLabel.text = "VR Controller SUCCESSFULLY connected."
+        //DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: {
+         //   self.informationLabel.text = "Focus on the centre of the screen when placing a box."
+       // })
         configureNotifications()
         
         guard let controller = notification.object as? DDController else { return }
@@ -169,49 +145,68 @@ extension ViewController {
         let touchpadMaxPoint: CGFloat = 250.0
         
         controller.touchpad.pointChangedHandler = { (touchpad: DDControllerTouchpad, point: CGPoint) in
-            //TODO
-            
-            if !self.lastPoint.equalTo(CGPoint.zero) {
-                //print("Touchpad was touched")
-                //print(point)
+            if !self.lastPoint.equalTo(CGPoint.zero) && self.firstPoint.equalTo(CGPoint.zero) {
+                self.firstPoint = point
+            }
+            print(self.lastPoi)
+            print(point)
+            if self.lastPoi.equalTo(CGPoint.zero) && point.equalTo(CGPoint.zero) {
+                self.lastPoi = self.lastPoint
+                // Swipe detection
+                print(self.firstPoint.x)
+                print(self.lastPoi.x)
+                if (self.firstPoint.x<127.0) && (self.lastPoi.x>180){
+                    //Swiped to the right
+                    print("User swiped to the right!")
+                    self.lastNode?.removeFromParentNode()
+                    self.createCube(changeMaterial: true, multiplier: 0)
+                }
+            }
+            if (point.equalTo(CGPoint.zero)){
+                self.lastPoi = CGPoint.zero
+                self.firstPoint = CGPoint.zero
             }
             self.lastPoint = point
+           // print(self.lastPoint)
+            
         }
         
         controller.touchpad.button.valueChangedHandler = { (button: DDControllerButton, pressed: Bool) in
             if pressed {
-                print("Touchpad was pressed")
+                //print("Touchpad was pressed")
             }
         }
         
         controller.appButton.valueChangedHandler = { (button: DDControllerButton, pressed: Bool) in
-            self.checking = ( self.checking.1 ,pressed ? true : false)
-            if self.checking.1 {
+            self.checking0 = ( self.checking0.1 ,pressed ? true : false, self.checking0.2)
+            if self.checking0.1 {
                 //Only interact with the object, if it's in the field of view.
-                if let pointOfView = self.sceneView.pointOfView {
-                    let isVisible = self.sceneView.isNode(self.lastNode!, insideFrustumOf: pointOfView)
-                    if isVisible {
-                        if self.first {
+              //  if let pointOfView = self.sceneView.pointOfView {
+                  //  let isVisible = self.sceneView.isNode(self.lastNode!, insideFrustumOf: pointOfView)
+                //    if isVisible {
+                        if self.checking0.2 {
                             //TODO: Either delete of move
                             self.lastNode?.removeFromParentNode()
                             self.createCube(changeMaterial: false, multiplier: 0.0)
-                            
-                            let point = CGPoint(x: self.sceneView.frame.size.width / 2, y: self.sceneView.frame.size.height / 2); //Get a point at the middle of the screen
+                            //Use the middle of the screen for hitTest
+                            let point = CGPoint(x: self.sceneView.frame.size.width / 2, y: self.sceneView.frame.size.height / 2);
                             let hitResults = self.sceneView.hitTest(point, types: .existingPlaneUsingExtent)
                             
                             if hitResults.count > 0, let firstHit = hitResults.first {
-                                print(Float(self.oldCubeSize.1/2))
-                                print(firstHit.worldTransform.columns.3.y)
                                 self.lastNode?.position = SCNVector3Make(firstHit.worldTransform.columns.3.x, firstHit.worldTransform.columns.3.y + Float(self.oldCubeSize.1/2), firstHit.worldTransform.columns.3.z)
+                                //Remove the plane node after an object has been placed on it.
+                                //DispatchQueue.main.asyncAfter(deadline: .now() + 1.5, execute: {
+                                self.planeNode.removeFromParentNode()
+                                self.oldCubeSize = (0.1,0.1,0.1 )
+                                self.createCube(changeMaterial: false, multiplier: 0)
+                              //  })
                             }
-                            print(self.lastNode?.position)
-                            print(self.planeNode.position)
                         }
-                    }
-                }
+                //    }
+               // }
             }
-            else  if (!self.checking.0 && !self.checking.1) {
-                self.first = true
+            else  if (!self.checking0.0 && !self.checking0.1) {
+                self.checking0.2 = true
             }
         }
         
@@ -222,44 +217,44 @@ extension ViewController {
         }
         
         controller.volumeUpButton.valueChangedHandler = { (button: DDControllerButton, pressed: Bool) in
-            self.checking = ( self.checking.1 ,pressed ? true : false)
+            self.checking = ( self.checking.1 ,pressed ? true : false, self.checking.2)
             if self.checking.1 {
                 //Only interact with the object, if it's in the field of view.
                 if let pointOfView = self.sceneView.pointOfView {
                     let isVisible = self.sceneView.isNode(self.lastNode!, insideFrustumOf: pointOfView)
                     if isVisible {
-                        if self.first {
+                        if self.checking.2 {
                             self.lastNode?.removeFromParentNode()
                             self.createCube(changeMaterial: false, multiplier: 0.1)
                             self.oldCubeSize = (self.oldCubeSize.0 + 0.1, self.oldCubeSize.1 + 0.1, self.oldCubeSize.2 + 0.1)
-                            self.first = false
+                            self.checking.2 = false
                         }
                     }
                 }
             }
             else  if (!self.checking.0 && !self.checking.1) {
-                self.first = true
+                self.checking.2 = true
             }
         }
         
         controller.volumeDownButton.valueChangedHandler = { (button: DDControllerButton, pressed: Bool) in
-            self.checking = ( self.checking.1 ,pressed ? true : false)
-            if self.checking.1 {
+            self.checking2 = ( self.checking2.1 ,pressed ? true : false, self.checking2.2)
+            if self.checking2.1 {
                 //Only interact with the object, if it's in the field of view.
                 if let pointOfView = self.sceneView.pointOfView {
                     let isVisible = self.sceneView.isNode(self.lastNode!, insideFrustumOf: pointOfView)
                     if isVisible {
-                        if self.first {
+                        if self.checking2.2 {
                             self.lastNode?.removeFromParentNode()
                             self.createCube(changeMaterial: false, multiplier: (-0.1))
                             self.oldCubeSize = (self.oldCubeSize.0 - 0.1, self.oldCubeSize.1 - 0.1, self.oldCubeSize.2 - 0.1)
-                            self.first = false
+                            self.checking2.2 = false
                         }
                     }
                 }
             }
-            else if (!self.checking.0 && !self.checking.1) {
-                self.first = true
+            else if (!self.checking2.0 && !self.checking2.1) {
+                self.checking2.2 = true
             }
         }
     }
